7f0a8bfa773bfdb6c91ed1205f2d23d5d8e50cb8
pure90719@gmail.com <pure90719@gmail.com>
2022-12-07 17:09:01 +0800

    add

---
+++ assets/head.ee063ba4.js
@@ -0,0 +1,1 @@
+const s="/127/assets/head.32ce6c28.jpeg";export{s as _};
---
+++ assets/useOnunmounted.73b7fc34.js
@@ -0,0 +1,1 @@
+import{i as o,g as n}from"./index.43ccde5d.js";const d=()=>o(()=>{n.to(document.body,{background:"#12181E",duration:0}),window.scroll(0,0)});export{d as b};
