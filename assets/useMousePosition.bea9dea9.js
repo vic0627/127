76711d767abb20ac8ee54700304704c01942ddfd
pure90719@gmail.com <pure90719@gmail.com>
2022-12-09 23:21:55 +0800

    aaaaa

---
+++ assets/useMousePosition.bea9dea9.js
@@ -0,0 +1,1 @@
+import{o as n,i as s}from"./index.ed1af488.js";const r="/assets/flying_drone.eccf8ef9.glb";let t,i;function u(d){const e=o=>{t=o.pageX,i=o.pageY};n(()=>{window.addEventListener("mousemove",e)}),s(()=>{window.removeEventListener("mousemove",e)})}export{r as d,u,t as x,i as y};
